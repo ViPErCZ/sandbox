f7f7dc649b670dd4cb60629e0a1343dfc5b08938
ViPErCZ <info@vipersoftware.net>
2016-03-25 10:16:28 +0100

    add tinymce init custom callback

--- www/js/app/componentForm.js
+++ www/js/app/componentForm.js
@@ -9,21 +9,22 @@
  * @version 2.0.0
  * @returns componentForms
  */
-var componentForms = function() {
-    var form;
-    var content;
-    var link;
-    var loader;
+var componentForms = function () {
+	var form;
+	var content;
+	var link;
+	var loader;
+	this.tinymceInitCustomCallback = null;
 
 	/**
 	 *
 	 */
-	var actions = function() {
+	var actions = function () {
 		var that = this;
 
 		/** Checkbox bolder */
-		form.find("input[type='checkbox']").each(function() {
-			$(this).on("click", function() {
+		form.find("input[type='checkbox']").each(function () {
+			$(this).on("click", function () {
 				if ($(this).prop("checked")) {
 					$(this).closest("label").css("font-weight", "bold");
 				} else {
@@ -32,45 +33,47 @@ var componentForms = function() {
 			});
 		});
 
-        /** Cancel */
-        form.find("#cancel").off("click").on("click", function(event) {
-            event.preventDefault();
+		/** Cancel */
+		form.find("#cancel").off("click").on("click", function (event) {
+			event.preventDefault();
 			$(".customError").alert('close');
 			var xhr = snippetCallback(that, link, this, event);
 			if (xhr) {
-				xhr.success(function() {
+				xhr.success(function () {
 					var bar = $.appForms.getActionBar();
 					if (bar) {
 						bar.enablePanel();
 					}
 				});
 			}
-        });
+		});
 
-        /** Submit form */
-        form.find("input[type=submit], button[type=submit]").off("click").on("click", function(event) {
-            event.preventDefault();
+		/** Submit form */
+		form.find("input[type=submit], button[type=submit]").off("click").on("click", function (event) {
+			event.preventDefault();
 			$(".customError").alert('close');
 			if (tinyMCE) {
-				$(tinymce.get()).each(function(i, el){
+				$(tinymce.get()).each(function (i, el) {
 					document.getElementById(el.id).value = el.getContent();
 				});
 			}
 
 			var xhr = $.nette.ajax({url: form.attr('action')}, this, event);
 			if (xhr) {
-				xhr.success(function(payload) {
+				xhr.success(function (payload) {
 					if (payload.result == "success") {
 						if (tinyMCE) {
-							$(tinymce.get()).each(function(i, el){
+							$(tinymce.get()).each(function (i, el) {
 								el.destroy();
 							});
 						}
 						var xhr = snippetCallback(that, link, form.find("#cancel"), event, {message: 'Úspěšně uloženo...'});
 						if (xhr) {
-							xhr.success(function() {
+							xhr.success(function () {
 								$(".customSuccess:first").removeClass('hidden').show();
-								setTimeout(function(){ $(".customSuccess:first").addClass('hidden') }, 1500);
+								setTimeout(function () {
+									$(".customSuccess:first").addClass('hidden')
+								}, 1500);
 								var bar = $.appForms.getActionBar();
 								if (bar) {
 									bar.enablePanel();
@@ -83,8 +86,8 @@ var componentForms = function() {
 					}
 				});
 			}
-        });
-    };
+		});
+	};
 
 	/**
 	 *
@@ -95,9 +98,9 @@ var componentForms = function() {
 	 * @param data
 	 * @returns {jqXHR|null}
 	 */
-	var snippetCallback = function(plugin, link, ui, e, data) {
+	var snippetCallback = function (plugin, link, ui, e, data) {
 		content.find("span:first").html('<div class="well center"><img src="' + loader + '" alt="loading..." /></div>');
-		return $.nette.ajax({url: link, data: data }, ui, e);
+		return $.nette.ajax({url: link, data: data}, ui, e);
 	};
 
 	/**
@@ -106,31 +109,32 @@ var componentForms = function() {
 	 * @param grid
 	 * @param preloader
 	 */
-    this.init = function(ui, grid, preloader) {
-        form = ui;
-        content = grid;
-        link = content.data('link');
-        loader = preloader;
+	this.init = function (ui, grid, preloader) {
+		form = ui;
+		content = grid;
+		link = content.data('link');
+		loader = preloader;
 
 		$(".alert").alert();
 		$('.customError').off('close.bs.alert').on('close.bs.alert', function () {
 			$(".customError:first").addClass('hidden');
 			return false;
 		});
-        $.nette.load();
-        form.find('input[type=text]:first').focus();
-        form.find("input[type='checkbox']").each(function() {
-            if ($(this).prop("checked"))
-                $(this).closest("label").css("font-weight", "bold");
-        });
-        actions(form, content, link, preloader);
+		$.nette.load();
+		form.find('input[type=text]:first').focus();
+		form.find("input[type='checkbox']").each(function () {
+			if ($(this).prop("checked"))
+				$(this).closest("label").css("font-weight", "bold");
+		});
+		actions(form, content, link, preloader);
 		$(".chosen-select").chosen();
 		if (tinymce) {
-            tinyMCE.editors=[]; // remove any existing references
+			tinyMCE.editors = []; // remove any existing references
 			tinymce.init({
-				selector:'.mce',
-                language : 'cs',
-        		toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | macros",
+				selector: '.mce',
+				language: 'cs',
+				init_instance_callback: typeof this.tinymceInitCustomCallback == "function" ? this.tinymceInitCustomCallback : '',
+				toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | macros",
 				plugins: [
 					"fullpage, advlist autolink lists link image charmap print preview anchor",
 					"searchreplace visualblocks code fullscreen",
